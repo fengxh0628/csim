1768b5d744f72ebd37868f3b987291d95e3c5134
fengxh0628 <fengxh0628@gmail.com>
2026-05-28 21:39:36 +0800

    change code path to relative in configs

---
+++ accepted/code/mstd.py
@@ -0,0 +1,30 @@
+"""Cross-sectional momentum alpha: N-period return up to current bar."""
+import numpy as np
+
+from modules.alphabase import AlphaBase
+from core.universe import univbase
+from core.utils import parse_duration
+from lib import fast
+
+
+class AlphaTest(AlphaBase):
+
+    def __init__(self, cfg: dict):
+        super().__init__(cfg)
+        self.data = self.dr.getdata(cfg.get('data', 'premiumIndexKlines.close'))
+        self.itvls = parse_duration(cfg.get('lookback', '2d'))
+        self.direction = int(cfg.get('direction', 1))
+
+    def generate(self, idx: int) -> None:
+        didx = idx - self.delay
+        valid = self.get_valid(idx)
+        start_idx = didx - self.itvls
+        end_idx = didx + 1
+
+        data = self.data[start_idx:end_idx, valid]
+
+        self.alpha[valid] = fast.niomean(data) / fast.niostd(data) * self.direction
+
+
+def create(cfg: dict) -> AlphaTest:
+    return AlphaTest(cfg)
---
+++ accepted/code/rskew.py
@@ -0,0 +1,34 @@
+"""Cross-sectional momentum alpha: N-period return up to current bar."""
+import numpy as np
+
+from modules.alphabase import AlphaBase
+from core.universe import univbase
+from core.utils import parse_duration
+from lib import fast
+
+
+class AlphaTest(AlphaBase):
+
+    def __init__(self, cfg: dict):
+        super().__init__(cfg)
+        self.data1 = self.dr.getdata(cfg.get('data1', 'volume'))
+        self.data2 = self.dr.getdata(cfg.get('data2', 'count'))
+        self.itvls = parse_duration(cfg.get('lookback', '3d'))
+        self.direction = int(cfg.get('direction', 1))
+
+    def generate(self, idx: int) -> None:
+        didx = idx - self.delay
+        valid = self.get_valid(idx)
+        start_idx = didx - self.itvls
+        end_idx = didx + 1
+
+        data1 = self.data1[start_idx:end_idx, valid]
+        data2 = self.data2[start_idx:end_idx, valid]
+
+        data = data1 / data2
+
+        self.alpha[valid] = fast.nioskew(data) * self.direction
+
+
+def create(cfg: dict) -> AlphaTest:
+    return AlphaTest(cfg)
